4beaacb512f6e171a073ef03006a4128c880c2b4
galeracris <cristian.galera@gmail.com>
2021-09-12 10:56:39 -0300

    se agregaron img y function-button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
+import Menu from './Menu';
+import List from './List';
 
-function App() {
+class App extends React.Component{
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      books:[
+        {id:0, rating: 4, title: 'Harry Potter y el cáliz de fuego', image: 'libro01.jpg'},
+        {id:1, rating: 3, title: 'The shining', image: 'libro02.jpg'},
+        {id:2, rating: 5, title: 'Código Da Vinci', image: 'libro03.jpg'},
+        {id:3, rating: 5, title: 'El Principito', image: 'libro04.jpg'},
+        {id:4, rating: 5, title: 'Sobrenatural', image: 'libro05.jpg'} 
+      ]
+    };
+  }
+
+
+  render(){
+
   return (
-    <div>
-      Libreria
+    <div className="app">
+      <Menu title="Libreria Online"/>
+      <List items={this.state.books} />
     </div>
   );
+  }
+
 }
 
 export default App;
